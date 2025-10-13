6444ef70328cc2ed93952a317e8887bff2e3908e
“Mandar <mandarmjog@gmail.com>
2025-10-13 18:15:22 +0530

    constructor demo

---
+++ Project1/src/Dog.java
@@ -0,0 +1,28 @@
+
+public class Dog {
+	private String dogName,dogBreed;
+	private int dogAge;
+	
+	public void displayDogInformation() {
+		System.out.println(this.dogName+" , "
+					+this.dogBreed+","+this.dogAge);
+	}
+	
+	//default constructor : invoked implicitly when we create object
+//	public Dog() {
+//		System.out.println("Default constructor invoked!!");
+//		this.dogName="Bruno";
+//		this.dogBreed="Husky";
+//		this.dogAge=2;
+//	}
+	
+	//parameterized constructor
+	public Dog(String dogName, String dogBreed, int dogAge) {
+		System.out.println("parameterizec constructor invoked !!!");
+		this.dogName=dogName;
+		this.dogBreed=dogBreed;
+		this.dogAge=dogAge;
+	}
+	
+
+}
--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -4,11 +4,18 @@ public class Main {
 		System.out.println("hello from java again  & again!");
 //		int[] data= {10,20,40,4,54,23,54};
 		
-		Employee emp1=new Employee();
-		emp1.initEmployeeData(101, "John", "Programming");
-		emp1.calculateSalary();
-		String empInfo=emp1.displayEmployee();
-		System.out.println(empInfo);
+//		Employee emp1=new Employee();
+//		emp1.initEmployeeData(101, "John", "Programming");
+//		emp1.calculateSalary();
+//		String empInfo=emp1.displayEmployee();
+//		System.out.println(empInfo);
+//		
+		
+		Dog dog1=new Dog("Husky","Bruno",2);
+		dog1.displayDogInformation();
+		
+		Dog dog2=new Dog("Pamerian","Neko",4);
+		dog2.displayDogInformation();
 		
 		
 	}
